139f910fc74e719622e8610703045cd656280aab
blackcoffee19 <cattuongw2000@gmail.com>
2022-05-16 20:26:54 +0700

    Add project files.

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Greeting()
+        {
+            List<string> greeting = new List<string>
+            {
+                "console.log(\"Hello, World!\");",
+                "Console.WriteLine(\"Hello, World!\")",
+                "std:cout<< \"Hello, World!\"",
+                "System.out.println(\"Hello, World!\")",
+                "echo \"Hello, World!\""
+            };
+            Random ran = new Random();
+            int num = ran.Next(0, greeting.Count);
+            return greeting[num];
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/Pages/Portfolio.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class Portfolio : PageModel
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public int Experients { get; set; }
+        public List<Language> languages { get; set; }
+        public void OnGet()
+        {
+            FullName = "Black Coffee";
+            Age = 22;
+            Experients = 6;
+            languages = new List<Language>
+            {
+                new Language("Java", 4, "Codecademy certificate"),
+                new Language("JavaScript", 4, "Codecademy certificate"),
+                new Language("C++", 2, "Codecademy certificate"),
+                new Language("C#", 4, "Codecademy certificate"),
+                new Language("SQL", 1, "Codecademy certificate"),
+                new Language("HTML", 2, "Codecademy certificate"),
+                new Language("CSS", 2, "Codecademy certificate")
+            };
+        }
+    }
+
+    public class Language
+    {
+        public string Name { get; set; }
+        public int exper { get; set; }
+        public string certificate { get; set; }
+        public Language(string name, int exper, string certi = "none")
+        {
+            this.Name = name;
+            this.exper = exper;
+            this.certificate = certi;
+        }
+    }
+}
